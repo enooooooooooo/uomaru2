6953367f4f0c732c59e4949c9013edc72fb26d5e
enooooooooooo <95466294+enooooooooooo@users.noreply.github.com>
2023-09-24 01:05:46 +0800

    Add files via upload

---
+++ uomaru/index.js
@@ -0,0 +1,11 @@
+$(window).scroll(function (){
+	$('.slidein').each(function(){
+		var elemPos = $(this).offset().top,
+		scroll = $(window).scrollTop(),
+		windowHeight = $(window).height();
+
+			if (scroll > elemPos - windowHeight + 150){
+				$(this).addClass('scrollin');
+			}
+	});
+});
